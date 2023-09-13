ca188c4960af8dd94c1ad2b8369c45917ced50c5
IgorRibeiro7 <igorbez.12@icloud.com>
2023-09-13 10:29:03 -0300

    pagina editar produtos finalizada

--- aula-rotas/src/routes/EditarProdutos/index.jsx
+++ aula-rotas/src/routes/EditarProdutos/index.jsx
@@ -1,8 +1,25 @@
+import {useParams, useNavigate } from "react-router-dom"
+import { listraProdutos } from "../../componentes/listaProdutos"
+
 export default function EditarProdutos(){
+
+    const lista = listaProdutos
+    const navegacao = useNavigate()
+    const {id} = useParams()
+
+    const proc = lista.filter(prod => prod.id == id)
+    const produto = proc [0]
+
+    const salvar = ()=>{
+        alert (`Produto: ${produto.nome} editado com sucesso!`)
+        retur navegacao ('/produtos')
+    }
 
     return(
         <main>
-            <h1>EditarProdutos</h1>
+            <h1>Editando o produto</h1>
+            <p> Editando os dados do produto:{produto.nome} </p>
+            <button onClick={salvar}></button>
         </main>
     )
 }
